617f1eb58b4bfce50b94d54a90248e1ee518f972
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-19 20:11:48 -0500

    Update pocket.py
    Made slight adjustments in class representation

--- pocket.py
+++ pocket.py
@@ -1,26 +1,25 @@
 # Holds the representation of a pocket
 class Pocket:
-    def __init__(self, numStones, player):
+    def __init__(self, type, numStones, player):
         self.numStones = numStones
         self.player = player
+        self.type = type
 
     def __str__(self):
-        return f"P({self.numStones}, {self.player}）"
+        return f"P({self.type}, {self.numStones}, {self.player}）"
 
     # Accessors
-    def pocket_stones(self):
+    def get_type(self):
+        return self.type
+    def get_stones(self):
         return self.numStones
-    def pocket_player(self):
+    def get_player(self):
         return self.player
 
     # Mutators
-    # Flexibly changes the number of stones in a pocket
-    def set_stones(self, stones):
-        self.numStones = stones
+    def empty(self):
+        self.numStones = 0
 
     # Adds one stone to the pocket (as in when it is "moved over" over the course of a move)
     def increment_stones(self):
         self.numStones += 1
-
-
-
