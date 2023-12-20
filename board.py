62b6fdffe2581e41c8a758e98a85f6951ad8e70e
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-19 20:09:29 -0500

    Add files via upload

--- board.py
+++ board.py
@@ -1,47 +1,73 @@
 import pocket
-import mancala
 
 class Board:
     # List to hold the representation of the mancala board
-    pockets = []
+    board = []
 
-    # The mancala board will be represented with a list of the 12 pockets and two
-    # independently initialized mancalas
+    # The mancala board will be represented with a list of length 14
+    # with 12 pockets and two mancalas
     def __init__(self):
 
-        #Populating board with two mancalas and 12 pockets, placed accordingly
+        # Populating board with two mancalas and 12 pockets, placed accordingly
         for i in range(12):
-            if (i < 6):
-                self.pockets.append(pocket.Pocket(4, 2))
+            if i < 6:
+                self.board.append(pocket.Pocket("pocket", 4, 1))
             else:
-                self.pockets.append(pocket.Pocket(4, 1))
+                self.board.append(pocket.Pocket("pocket", 4, 2))
 
         # Initializing the two mancalas
-        self.mancala_one = mancala.Mancala(0, 1)
-        self.mancala_two = mancala.Mancala(0, 2)
+        self.board.insert(6, pocket.Pocket("mancala", 0, 1))
+        self.board.insert(0, pocket.Pocket("mancala", 0, 2))
 
-
+    def __str__(self):
+        result = ""
+        for pocket in self.board:
+            result += " " + str(pocket)
+        return result
     def move(self, player, position):
 
-        # Invalid move checking (out of bounds, wrong player, empty pocket)
+        # Checking to make sure the move is in bounds
         if position < 0 or position > 12:
             raise Exception("Invalid Move: Position out of bounds (0-11)")
 
-        current_pocket = pocket[position]
-        if current_pocket.pocket_player() != player:
+        current_pocket = self.board[position]
+
+        # Checking to make sure the move is not from a mancala, the pocket does not belong to the
+        # other player, and the pocket is not empty
+        if current_pocket.get_type == "mancala":
+            raise Exception("Invalid Move: Cannot move from a mancala")
+        if current_pocket.get_player() != player:
             raise Exception("Invalid Move: Pocket belongs to other player")
-        if current_pocket.pocket_stones == 0:
+        if current_pocket.get_stones() == 0:
             raise Exception("Invalid Move: Pocket is empty")
 
         # Accessing the number of stones we have to move with and setting the number of
         # stones in the originating pocket to 0 to start the move
-        num_stones = current_pocket.pocket_stones
-        current_pocket.set_stones(0)
+        num_stones = current_pocket.get_stones()
+        current_pocket.empty()
 
         for i in range(num_stones):
 
             # Iterating through the pockets in the board
-            current_pocket = self.pockets[position + 1 + i]
+            current_pocket = self.board[position + 1 + i]
+            current_pocket.increment_stones()
+
+    # Function to check if the game is finished
+    # The game is finished when all the pockets have 0 stones
+    def check_end(self):
+        for i in range(14):
+            if i == 0 or i == 7:
+                continue
+            else:
+                if self.board[i].get_stones() != 0:
+                    return False
+        return True
+
+
+
+
+
+
 
 
 
