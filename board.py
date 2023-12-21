035d5f85984fa93e764334dcdeeba2c827c53792
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-20 20:57:58 -0500

    Modifications (slightly buggy)
    Double prints the board when a player lands in one of their mancalas with stones in it

--- board.py
+++ board.py
@@ -1,3 +1,10 @@
+"""
+Name: board.py
+Author: Eric Cui
+Created: December 2023
+Description: Contains the Board class. The Board class contains all the logic for game representation,
+move initiation, and game-state-checking
+"""
 import pocket
 
 
@@ -60,29 +67,62 @@ class Board:
         if print_type == "simple":
             print(self.simple_string())
 
-    def move(self, player, position):
+    def valid_move(self, player, position):
         """
-        Performs a mancala cascade move with built-in illegal move checking
+        Checks if inputted move is valid
         :param player: (int) The player moving. 1 indicates player1, 2 indicates player2
         :param position: (int) Indicates which pocket to move from. It is the index of the pocket
-        (note: mancalas are illegal to move from)
-        :return: (void)
+        :return:
         """
 
         # Checking to make sure the move is in bounds
         if position < 0 or position > 12:
-            raise Exception("Invalid Move: Position out of bounds")
+            print("Invalid position")
+            return False
 
         current_pocket = self.board[position]
 
         # Checking to make sure the move is not from a mancala, the pocket does not belong to the
         # other player, and the pocket is not empty
         if current_pocket.get_type() == "mancala":
-            raise Exception("Invalid Move: Cannot move from a mancala")
+            print(f"Pocket type = {current_pocket.get_type()}")
+            return False
         if current_pocket.get_player() != player:
-            raise Exception("Invalid Move: Pocket belongs to other player")
+            print(f"Pocket player = {current_pocket.get_player()}")
+            return False
         if current_pocket.get_stones() == 0:
-            raise Exception("Invalid Move: Pocket is empty")
+            print(f"Pocket stones = {current_pocket.get_stones()}")
+            return False
+
+        # Move is valid if other cases do not fire
+        return True
+
+    def empty_side(self, player):
+        """
+        Checks if a player's side is empty (meaning they have no valid moves)
+        :param player: (int) The player moving. 1 indicates player1, 2 indicates player2
+        :return: (bool) False if the player's side is not empty, True if the player's side is empty
+        """
+        if player == 1:
+            for i in range(6):
+                if self.board[i].get_stones() != 0:
+                    return False
+        else:
+            for i in range(7, 13):
+                if self.board[i].get_stones() != 0:
+                    return False
+        return True
+
+    def move(self, player, position):
+        """
+        Performs a mancala cascade move and tracks whether the player landed in their own mancala
+        :param player: (int) The player moving. 1 indicates player1, 2 indicates player2
+        :param position: (int) Indicates which pocket to move from. It is the index of the pocket
+        (note: mancalas are illegal to move from)
+        :return: (bool) True if the player's final move is in their own mancala, false if not
+        """
+
+        current_pocket = self.board[position]
 
         # Accessing the number of stones we have to move with and setting the number of
         # stones in the originating pocket to 0 to start the move
@@ -91,7 +131,6 @@ class Board:
 
         # Iterating through the pockets in the board and incrementing accordingly
         for i in range(num_stones):
-
             # Uses modulus to ensure the moves properly wrap around
             current_pocket = self.board[(position + 1 + i) % 14]
             current_pocket.increment_stones()
@@ -99,18 +138,56 @@ class Board:
         # Tracking the ending pocket to determine whether the move continues
         end_pocket = self.board[current_pocket.get_position()]
 
-        # Move continues if the ending pocket belongs to the player and it is not a mancala
-        if end_pocket.get_player() == player and end_pocket.get_type() == "pocket":
+        # Move continues if the ending pocket belongs to the player, it is not a mancala, and it has more than one stone
+        if end_pocket.get_player() == player and end_pocket.get_type() == "pocket" and end_pocket.get_stones() > 1:
+            self.print_board("simple")
             self.move(player, end_pocket.get_position())
 
+        # Player is allowed another move if their final move lands in their mancala
+        if end_pocket.get_player() == player and end_pocket.get_type() == "mancala":
+            self.print_board("simple")
+            return True
+
+        self.print_board("simple")
+        return False
 
     # Function to check if the game is finished
     # The game is finished when all the pockets have 0 stones
     def check_end(self):
         for i in range(14):
-            if i == 0 or i == 7:
+            if i == 6 or i == 13:
                 continue
             else:
                 if self.board[i].get_stones() != 0:
                     return False
         return True
+
+    def set_board_state(self, state):
+        """
+        Sets each pocket and mancala on the board to contain a certain number of stones
+        :param state: (str) A string of 14 numbers separated by spaces. Each number represents the
+        number of stones in the pocket at the corresponding "index" of the number
+        :return: (void)
+        """
+
+        # Splitting the string
+        split_string = state.split()
+
+        # Container for the stone counts in the form of (ints)'s
+        stone_counts = []
+
+        # Adding the stone counts
+        for char in split_string:
+            stone_counts.append(int(char))
+
+        # Assigning the stone counts
+        for i in range(14):
+            self.board[i].set_stones(stone_counts[i])
+
+    def switch_player(self, player):
+        if player == 1:
+            return 2
+        else:
+            return 1
+
+
