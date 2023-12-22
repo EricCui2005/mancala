29ea20b96fb5ebf2e4d48483a32601f92e732f56
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-21 20:26:36 -0500

    Formatting and commenting

--- pocket.py
+++ pocket.py
@@ -6,7 +6,11 @@ Description: Contains the Pocket class. The Pocket class contains all the logic 
 representation and manipulation
 """
 class Pocket:
+    """
+    The Pocket class contains all the logic for representing and manipulating a playable mancala pocket/mancala
+    """
     def __init__(self, pocket_type, num_stones, player, position):
+        
         # (int) Number of stones in the pocket
         self.num_stones = num_stones
 
@@ -21,14 +25,21 @@ class Pocket:
         # (int) The position of the pocket on board (0-13)
         self.position = position
 
-    # Returns a comprehensive string representation describing all the pocket's information
     def __str__(self):
-
+        """
+        Returns a comprehensive string representation describing a pocket's type, the number of stones it 
+        contains, the player to which it belongs, and its position.
+        :return: 
+        """
+        
         # Returns a point-like string of the form (pocket_type, num_stones, player, position)
         return f"({self.type}, {self.num_stones}, {self.player}, {self.position}）"
 
-    # Returns a simple representation of the pocket depicting the number of stones it has
     def simple_string(self):
+        """
+        Returns a simple representation of the pocket describing only the number of stones it contains
+        :return: 
+        """
         return f"({self.num_stones})"
 
     # Accessors
@@ -49,6 +60,6 @@ class Pocket:
     def increment_stones(self):
         self.num_stones += 1
 
-    # Sets the stone in the pocket
+    # Sets the number of stones in the pocket
     def set_stones(self, stone_count):
         self.num_stones = stone_count
