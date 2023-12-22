7f10a6b7832bd10be73bce20f865092ad03df080
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-21 20:17:23 -0500

    Vastly simplified double-move checking (moving player landed in their own mancala)

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ def play_game(play_board):
     """
     Mancala cascade game loop
     :param play_board: (Board) Takes in a board object
-    :return: (void)
+    :return: (int) Returns 1 if player1 wins, 2 if player2 wins, and 0 if it is a draw
     """
 
     # Initializing the play board and printing out its initial state
@@ -24,6 +24,10 @@ def play_game(play_board):
     # Game loop
     while not play_board.check_end():
 
+        # Switches the player if the current player has no valid moves
+        if play_board.empty_side(current_player):
+            current_player= play_board.switch_player(current_player)
+
         # Information output and move input gathering
         print(f"Player {current_player}'s Move")
         move_position = int(input("Move: "))
@@ -33,45 +37,25 @@ def play_game(play_board):
             print("Invalid move")
             move_position = int(input("Move: "))
 
-        # We first check if the moving player has landed in their mancala
-        # We evaluate if the moving player has valid moves in this case
+        # If the moving player landed in their mancala (they are allowed to move again), we do not switch the player
         if play_board.move(current_player, move_position):
+            continue
 
-            # If the player landed in their mancala, we check if they have
-            # any valid moves left
-            if play_board.empty_side(current_player):
-
-                # We change players if the current moving player landed in their own mancala but
-                # does not have any valid moves left (their side is empty)
-                current_player = play_board.switch_player(current_player)
-            else:
-
-                # If the player lands in their mancala, and they have valid moves, we print the board
-                # and allow the player to move again (through the continue statement)
-                continue
-
-        # We evaluate if the other player has valid moves if the current moving player did not land
-        # in their own mancala
+        # We switch players otherwise
         else:
-            current_player = play_board.switch_player(current_player)
-
-            # If the moving player did not land in their own mancala but the opposite player has
-            # no valid moves, we allow the current moving player to continue moving
-            if play_board.empty_side(current_player):
-                current_player = play_board.switch_player(current_player)
-                continue
-            else:
-                # We change players if the current moving player did not land in their own mancala
-                continue
+            play_board.switch_player(current_player)
 
     # Game end and winner output
     print("Game end")
     if play_board.board[6].get_stones() > play_board.board[13].get_stones():
         print("Player1 Wins")
+        return 1
     elif play_board.board[6].get_stones() < play_board.board[13].get_stones():
         print("Player2 Wins")
+        return 2
     else:
         print("Draw")
+        return 0
 
 
 
