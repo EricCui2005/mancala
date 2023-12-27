f56f27810717c76e2e75e0d837e9ab4a629c2ed9
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-26 20:23:12 -0500

    Added basic task controlling

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
+"""
+Name: main.py
+Author: Eric Cui
+Created: December 2023
+Description: This is the main file for the mancala project
+"""
+import game
 import board
+import boardGUI
+import randomgame
+
 
 def main():
-    playBoard = board.Board()
-    for elem in playBoard.pockets:
-        print(elem)
-    print(len(playBoard.pockets))
+
+    # Initializing the game board
+    play_board = board.Board()
+
+    # Outputting and receiving task selection
+    print("(1): Two player console game")
+    print("(2): Two player GUI game")
+    print("(3): Random vs random trials")
+    selection = int(input("Select task: "))
+
+    # BoardGUI selection
+    if selection == 1:
+        game.play_game(play_board)
+    if selection == 2:
+        bGUI = boardGUI.BoardGUI(play_board, 1)
+    if selection == 3:
+        random_trials_routine()
+
+def random_trials_routine():
+    """
+    Runs random trials and outputs information
+    :return: (void) Simply prints information
+    """
+    num_trials = int(input("Enter number of trials: "))
+    win_data = randomgame.random_game_trials(num_trials)
+    print(win_data)
+
 
 if __name__ == "__main__":
     main()
