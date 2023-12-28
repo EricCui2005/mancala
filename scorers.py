b1a6ed0d54581e6e0448b238ca2d322c8eb34c29
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-27 20:23:40 -0500

    Update scorers.py

--- scorers.py
+++ scorers.py
@@ -5,15 +5,12 @@ Created: December 2023
 Description: Contains the board evaluation routines that will be fed into the minimax algorithm
 """
 
-
-class Scorers:
-
-    def simple_score(self, play_board, player):
-        """
-        Returns the simplest evaluation of the board state. It subtracts the moving player's mancala stones from
-        the opposing player's mancala stones. Positive values are defined as more favorable for the moving player
-        :param player: (int) The moving player. 1 for player1, 2 for player2
-        :param play_board: (Board) An internal representation of the current state of the board
-        :return: (int) The difference between the moving player's and opposing player's mancala stone count
-        """
-        return play_board.mancala_count(player) - play_board.mancala_count(play_board.switch_player(player))
+def simple_score(self, play_board, player):
+    """
+    Returns the simplest evaluation of the board state. It subtracts the moving player's mancala stones from
+    the opposing player's mancala stones. Positive values are defined as more favorable for the moving player
+    :param player: (int) The moving player. 1 for player1, 2 for player2
+    :param play_board: (Board) An internal representation of the current state of the board
+    :return: (int) The difference between the moving player's and opposing player's mancala stone count
+    """
+    return play_board.mancala_count(player) - play_board.mancala_count(play_board.switch_player(player))
