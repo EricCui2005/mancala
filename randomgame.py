a9979ae750184596eb062031828bf8f33aa6c507
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-27 20:27:06 -0500

    Changed to the hub for AI vs AI game tests

--- randomgame.py
+++ randomgame.py
@@ -1,5 +1,8 @@
 import randomAI
 import board
+import copy
+import smartplayer
+import scorers
 
 
 def randomGame(play_board):
@@ -35,7 +38,51 @@ def randomGame(play_board):
     else:
         return 0
 
+def random_vs_simple(play_board):
 
+    randy = randomAI.RandomAI()
+    sam = smartplayer.smart_player()
+
+    # SimpleAI is defined to be player1
+    current_player = 1
+
+    # Game loop
+    while not play_board.check_end():
+
+        # Switch players if the current player has no moves
+        if play_board.empty_side(current_player):
+            current_player = play_board.switch_player(current_player)
+
+        # SimpleAI moves if current_player is 1
+        if current_player == 1:
+
+            # Checks if the SimpleAI landed in their mancala
+            if play_board.move(1, sam.MM_best_move(play_board, 1, 3, scorers.simple_score), False):
+                continue
+
+            # We switch players if the SimpleAI did not land in their mancala
+            else:
+                play_board.switch_player(current_player)
+
+        # Random player moves if current_player is 2
+        else:
+
+            # Checks if the random player landed in their mancala
+            if randy.random_move(play_board, 2):
+                continue
+
+            # We switch players if the random player did not land in their mancala
+            else:
+                play_board.switch_player(current_player)
+                
+    # Game end
+    if play_board.board[6].get_stones() > play_board.board[13].get_stones():
+        return 1
+    elif play_board.board[6].get_stones() < play_board.board[13].get_stones():
+        return 2
+    else:
+        return 0
+    
 def random_game_trials(num_trials):
     """
     Runs a random vs random game trials
@@ -46,10 +93,11 @@ def random_game_trials(num_trials):
 
     # List to contain win information
     win_data = [0, 0, 0]
+    default_board = board.Board()
 
     # Executing trials according to num_trials
     for i in range(num_trials):
-        play_board = board.Board()
+        play_board = copy.deepcopy(default_board)
         result = randomGame(play_board)
         if result == 1:
             win_data[0] += 1
