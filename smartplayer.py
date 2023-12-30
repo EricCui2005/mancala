7e6c04d2e3f14aef17c3cc04b35b85103fed603e
Eric Cui <63880964+EricCui2005@users.noreply.github.com>
2023-12-30 18:38:16 -0500

    Added "prev_move" parameter to recursive functions
    "prev_move" is used to determine whether or not the move resulted in the moving player landing in their mancala

--- smartplayer.py
+++ smartplayer.py
@@ -4,11 +4,13 @@ import copy
 
 class smart_player():
 
-    def minimax(self, play_board, depth, maximizing, scorer, player):
+    def minimax(self, play_board, depth, maximizing, scorer, player, prev_move):
         """
         Recursively returns the value of the best possible board state for a certain player given a
         current board state and assuming the opponent moves optimally. The current moving player
         is defined to be the maximizer who is seeking to maximize the value of the board state
+        :param prev_move: (Tuple(int, int)) Information about the previous move for the purposes of 
+        tracking if the previous move results in us landing in a mancala
         :param play_board: (Board) The current board position being evaluated
         :param depth: (int) The remaining number of "moves into the future" the function considers
         :param maximizing: (bool) Describes whether it is the maximizing player's turn or not
@@ -21,7 +23,7 @@ class smart_player():
 
         # Base case
         if play_board.check_end() or depth == 0 or play_board.empty_side(player):
-            return scorer(play_board, player)
+            return scorer(play_board, player, prev_move)
 
         # Switch player before making a move
         current_player = player if maximizing else play_board.switch_player(player)
@@ -29,17 +31,32 @@ class smart_player():
         if maximizing:
             max_eval = float('-inf')
             for position in play_board.get_valid_moves(current_player):
+                
+                # Copying the board
                 new_board = copy.deepcopy(play_board)
+                
+                # Storing the move's position and the stones the pocket at that position holds
+                prev_move = (position, play_board.board[position].get_stones())
+                
+                # Making the move and evaluating using minimax
                 new_board.move(current_player, position, False)
-                score = self.minimax(new_board, depth - 1, False, scorer, current_player)
+                score = self.minimax(new_board, depth - 1, False, scorer, current_player, prev_move)
                 max_eval = max(max_eval, score)
             return max_eval
         else:
             min_eval = float('inf')
             for position in play_board.get_valid_moves(current_player):
+                
+                # Copying the board
                 new_board = copy.deepcopy(play_board)
+                
+                # Storing the move's position and the stones the pocket at that position holds
+                prev_move = (position, play_board.board[position].get_stones())
+                
+                # Making the move and evaluating using minimax
                 new_board.move(current_player, position, False)
-                score = self.minimax(new_board, depth - 1, True, scorer, current_player)
+                
+                score = self.minimax(new_board, depth - 1, True, scorer, current_player, prev_move)
                 min_eval = min(min_eval, score)
             return min_eval
 
@@ -66,7 +83,8 @@ class smart_player():
             new_board.move(player, position, False)
 
             # Returning the highest possible move from the position obtained from the current move
-            score = self.minimax(new_board, depth, True, scorer, player)
+            # NOTE: prev_move is initially none
+            score = self.minimax(new_board, depth, True, scorer, player, None)
 
             # Appropriately updating max_score and best_move
             if score > max_score:
